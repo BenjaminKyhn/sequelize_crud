655cf132dc51c546cf50b9010620af5ba6441fad
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-04 15:18:13 +0100

    R in CRUD

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,59 @@ const createPost = async (req, res) => {
     }
 }
 
+const getAllPosts = async (req, res) => {
+    try {
+        const posts = await models.Post.findAll({
+            include: [
+                {
+                    model: models.Comment,
+                    as: 'comments'
+                },
+                {
+                    model: models.User,
+                    as: 'author'
+                }
+            ]
+        });
+        return res.status(200).json({ posts });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const getPostById = async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const post = await models.Post.findOne({
+            where: { id: postId },
+            include: [
+                {
+                    model: models.Comment,
+                    as: 'comments',
+                    include: [
+                        {
+                            model: models.User,
+                            as: 'author',
+                        }
+                    ]
+                },
+                {
+                    model: models.User,
+                    as: 'author'
+                }
+            ]
+        });
+        if (post) {
+            return res.status(200).json({ post });
+        }
+        return res.status(404).send('Post with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
 module.exports = {
     createPost,
+    getAllPosts,
+    getPostById,
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/', (req, res) => res.send('Welcome'))
 
 router.post('/posts', controllers.createPost);
+router.get('/posts', controllers.getAllPosts);
+router.get('/posts/:postId', controllers.getPostById);
 
 module.exports = router;
