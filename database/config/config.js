c1bd1e53f050e49235de74b33cb6ec3c3bc3fd4d
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-04 11:34:05 +0100

    Initial commit

---
+++ database/config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'sqlite',
+    storage: './database.sqlite3',
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'sqlite',
+    storage: ':memory',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'sqlite',
+    storage: './database.sqlite3',
+  },
+}
